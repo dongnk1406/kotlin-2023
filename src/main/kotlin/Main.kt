f20286d1751129f86b249c6259740333c526bd1c
dongp6 <tryourbest146@gmail.com>
2023-01-14 22:46:38 +0700

    init source

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    println("Hello World!");
+    var x = 5;
+    println("x = $x");
+    var y:Int = 20;
+    println("x = $y");
+    println("Program arguments: ${args.joinToString()}")
+}
